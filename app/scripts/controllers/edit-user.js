2cfc9c220eaf6d8071cdf367347060ea7451fc17
Sanjay Patel <skpatel20@gmail.com>
2017-03-19 07:55:05 +0530

    Replaced update user PUT to PATCH

--- app/scripts/controllers/edit-user.js
+++ app/scripts/controllers/edit-user.js
@@ -14,8 +14,17 @@ angular.module('angularSampleApp')
 
     $scope.update = function() {
 
-      formService.submit($scope.form, '/api/core/users/' + $scope.user.id, 'put', {
-        data: $scope.user,
+      var editedUser = $scope.user;
+
+      var patch = [
+          {"op": "replace", "path": "/name", "value": editedUser.name},
+          {"op": "replace", "path": "/unverified", "value": editedUser.unverified},
+          {"op": "replace", "path": "/blocked", "value": editedUser.blocked},
+          {"op": "replace", "path": "/admin", "value": editedUser.admin}
+      ];
+
+      formService.submit($scope.form, '/api/core/users/' + $scope.user.id, 'patch', {
+        data: patch,
         successMessage: 'Profile updated',
         onSuccess: function(currentUser){
           $location.url("/users/" + $routeParams.id);
